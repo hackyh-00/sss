010c725c8b620639a6ee32c21d6bac4c4b1e53f0
hackyh-00 <159104329+hackyh-00@users.noreply.github.com>
2024-06-12 21:06:53 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const express = require('express');
 const axios = require('axios');
-const app = express();
 
-app.get('/ss', async (req, res) => {
-  const url = req.query.url;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/ss', (req, res) => {
+  const { url } = req.query;
 
   if (!url) {
     return res.status(400).json({ error: 'URL parameter is required' });
   }
 
-  const thumbioUrl = `https://image.thum.io/get/width/1920/crop/400/fullpage/noanimate/${url}`;
+  const screenshotAPIUrl = `https://api.screenshotone.com/take?access_key=BEVtsJFIxH1Q4w&url=${encodeURIComponent(url)}&viewport_width=1920&viewport_height=1080&device_scale_factor=1&image_quality=80&format=jpg&block_ads=true&block_cookie_banners=true&full_page=false&block_trackers=true&block_banners_by_heuristics=false&delay=0&timeout=60`;
 
-  try {
-    const response = await axios.get(thumbioUrl, { responseType: 'stream' });
-    // Pipe the image stream directly to the response
-    response.data.pipe(res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred while fetching the image' });
-  }
+  axios.get(screenshotAPIUrl, { responseType: 'arraybuffer' })
+    .then(response => {
+      const contentType = response.headers['content-type'];
+      if (contentType && contentType.startsWith('image')) {
+        res.set('Content-Type', contentType);
+        res.send(response.data);
+      } else {
+        console.error('Invalid or missing Content-Type header in the response:', contentType);
+        res.status(500).json({ error: 'Invalid or missing Content-Type header in the response' });
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching screenshot:', error);
+      res.status(500).json({ error: 'An error occurred while fetching the screenshot' });
+    });
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
