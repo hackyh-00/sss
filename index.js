fcf63fb8caa7a69994d316660f316bbc94db2913
hackyh-00 <159104329+hackyh-00@users.noreply.github.com>
2024-06-13 09:49:39 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 const express = require('express');
 const axios = require('axios');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/ss', (req, res) => {
-  const { url } = req.query;
+app.get('/ss', async (req, res) => {
+  const url = req.query.url;
 
   if (!url) {
     return res.status(400).json({ error: 'URL parameter is required' });
   }
 
-  const screenshotAPIUrl = `https://api.screenshotone.com/take?access_key=zMccWr1hFTRAyg&url=${encodeURIComponent(url)}&viewport_width=1920&viewport_height=1080&device_scale_factor=1&image_quality=80&format=jpg&block_ads=true&block_cookie_banners=true&full_page=false&block_trackers=true&block_banners_by_heuristics=false&delay=0&timeout=60`;
+  const options = {
+    method: 'GET',
+    url: 'https://web-capture2.p.rapidapi.com/image',
+    params: {
+      url: url,
+      height: '780',
+      width: '1024'
+    },
+    headers: {
+      'X-RapidAPI-Key': 'b38444b5b7mshc6ce6bcd5c9e446p154fa1jsn7bbcfb025b3b',
+      'X-RapidAPI-Host': 'web-capture2.p.rapidapi.com'
+    },
+    responseType: 'stream' // specify response type as stream to directly pipe it to the client
+  };
 
-  axios.get(screenshotAPIUrl, { responseType: 'arraybuffer' })
-    .then(response => {
-      const contentType = response.headers['content-type'];
-      if (contentType && contentType.startsWith('image')) {
-        res.set('Content-Type', contentType);
-        res.send(response.data);
-      } else {
-        console.error('Invalid or missing Content-Type header in the response:', contentType);
-        res.status(500).json({ error: 'Invalid or missing Content-Type header in the response' });
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching screenshot:', error);
-      res.status(500).json({ error: 'An error occurred while fetching the screenshot' });
-    });
+  try {
+    const response = await axios.request(options);
+    // Pipe the image stream directly to the response
+    response.data.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while fetching the image' });
+  }
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
